a8b5da819dbd529fe8a0ab6af6212bc4d9660dc1
TigerStrike6026 <tigerstrike6026@gmail.com>
2020-02-01 10:44:09 -0600

    adding target constants and biggest block method

--- Test_Platform/src/main/java/frc/robot/PixyController.java
+++ Test_Platform/src/main/java/frc/robot/PixyController.java
@@ -1,9 +1,17 @@
 package frc.robot;
 
+import java.util.ArrayList;
+
 import io.github.pseudoresonance.pixy2api.Pixy2;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC;
 import io.github.pseudoresonance.pixy2api.Pixy2.LinkType;
+import io.github.pseudoresonance.pixy2api.Pixy2CCC.Block;
 
 public class PixyController{
+    private final int PixyWidth = 316;
+    private final int PixyHeight = 208;
+    private final int targetXcenter = 158;
+    private final int targetYcenter = 104;
     private final Pixy2 _pixy = Pixy2.createInstance(LinkType.SPI);
 
     public void init(){
@@ -17,6 +25,29 @@ public class PixyController{
      * in the second slot.
      */
     public double[] trackBall() {
-
+        var biggestBlock = getBiggestBlock();
+        biggestBlock.getAngle();
     }
+
+
+    private Block getBiggestBlock() {
+		// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
+		// does not wait for new data if none is available,
+		// and limits the number of returned blocks to 25, for a slight increase in efficiency
+		int blockCount = _pixy.getCCC().getBlocks(false, Pixy2CCC.CCC_SIG1, 25);
+		System.out.println("Found " + blockCount + " blocks!"); // Reports number of blocks found
+		if (blockCount <= 0) {
+			return null; // If blocks were not found, stop processing
+		}
+		ArrayList<Block> blocks = _pixy.getCCC().getBlocks(); // Gets a list of all blocks found by the Pixy2
+		Block largestBlock = null;
+		for (Block block : blocks) { // Loops through all blocks and finds the widest one
+			if (largestBlock == null) {
+				largestBlock = block;
+			} else if (block.getWidth() > largestBlock.getWidth()) {
+				largestBlock = block;
+			}
+		}
+		return largestBlock;
+	}
 }
