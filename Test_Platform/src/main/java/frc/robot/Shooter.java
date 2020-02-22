eb392c4cc2f51d17dcce5b058a754635a4778d14
TigerStrike6026 <tigerstrike6026@gmail.com>
2020-02-22 10:15:24 -0600

    Starting motor initialization for shooter.

--- Test_Platform/src/main/java/frc/robot/Shooter.java
+++ Test_Platform/src/main/java/frc/robot/Shooter.java
@@ -1,7 +1,55 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 public class Shooter {
+    private final WPI_TalonSRX _top = new WPI_TalonSRX(99);
+    private final WPI_TalonSRX _bottom = new WPI_TalonSRX(98);
+    private final int _pidSlot = 0;
+    private final int _timeoutMs = 30;
+    private final double _p = 0.2;
+    private final double _i = 0;
+    private final double _d = 0;
+    private final double _f = 0.2;
+    private final double _iZone = 100;
+    private final double _peakOutput = 0.5;
+
     public void init(){
+        _top.configFactoryDefault();
+        _bottom.configFactoryDefault();
+
+        _top.setNeutralMode(NeutralMode.Brake);
+        _bottom.setNeutralMode(NeutralMode.Brake);
+
+        _top.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, _pidSlot, _timeoutMs);
+        _bottom.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, _pidSlot, _timeoutMs);
+
+        _top.setInverted(false);
+        _top.setSensorPhase(false);
+        _bottom.setInverted(false);
+        _bottom.setSensorPhase(false);
+
+        _top.configNominalOutputForward(0, _timeoutMs);
+        _top.configNominalOutputReverse(0, _timeoutMs);
+        _top.configPeakOutputForward(_peakOutput, _timeoutMs);
+        _top.configPeakOutputReverse(-_peakOutput, _timeoutMs);
+
+        _bottom.configNominalOutputForward(0, _timeoutMs);
+        _bottom.configNominalOutputReverse(0, _timeoutMs);
+        _bottom.configPeakOutputForward(_peakOutput, _timeoutMs);
+        _bottom.configPeakOutputReverse(-_peakOutput, _timeoutMs);
+
+        _top.config_kF(_pidSlot, _f, _timeoutMs);
+        _top.config_kP(_pidSlot, _p, _timeoutMs);
+        _top.config_kI(_pidSlot, _i, _timeoutMs);
+        _top.config_kD(_pidSlot, _d, _timeoutMs);
+
+        _bottom.config_kF(_pidSlot, _f, _timeoutMs);
+        _bottom.config_kP(_pidSlot, _p, _timeoutMs);
+        _bottom.config_kI(_pidSlot, _i, _timeoutMs);
+        _bottom.config_kD(_pidSlot, _d, _timeoutMs);
 
     }
 
@@ -10,7 +58,7 @@ public class Shooter {
      * @param power power output 0 to 1
      */
     public void fire(double power){
-
+        
     }
 
     public boolean isAtSetPower(double power){
@@ -21,7 +69,7 @@ public class Shooter {
      * Opens the gate
      */
     public void openGate(){
-
+        
     }
 
     /**
