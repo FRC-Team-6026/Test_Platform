417767fe445ee0123211f7693c9ecaa5749df659
TigerStrike6026 <tigerstrike6026@gmail.com>
2020-02-27 16:42:33 -0600

    Adding color sensor to conveyor

--- Test_Platform/src/main/java/frc/robot/Conveyor.java
+++ Test_Platform/src/main/java/frc/robot/Conveyor.java
@@ -1,17 +1,30 @@
 package frc.robot;
 
+import com.revrobotics.ColorSensorV3;
+
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Conveyor {
-    private final AnalogInput _ultraSonic = new AnalogInput(0);
+    private final I2C.Port _i2cPort = I2C.Port.kOnboard;
+    private final ColorSensorV3 _colorSensor = new ColorSensorV3(_i2cPort);
 
     public void init(){
-        _ultraSonic.initAccumulator();
     }
 
     public boolean isBallInLoadingPosition(){
-        SmartDashboard.putNumber("Ultrasonic Output", _ultraSonic.getVoltage());
+        var r = _colorSensor.getRed();
+        var g = _colorSensor.getGreen();
+        var b = _colorSensor.getBlue();
+        var x = _colorSensor.getProximity();
+        SmartDashboard.putNumber("red", r);
+        SmartDashboard.putNumber("green", g);
+        SmartDashboard.putNumber("blue", b);
+        SmartDashboard.putNumber("proximity", x);
+        if (x > 115 && r > 150 && g > 150){
+            return true;
+        }
         return false;
     }
 
